74b85cdbbe8949e681298d6f6fb38d85e2fe97cf
reddysomula6789@gmail.com <reddysomula6789@gmail.com>
2023-08-08 21:57:15 +0530

    byid

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -10,6 +10,22 @@ exports.getAllUsers = async (req, res) => {
   }
 };
 
+exports.getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id; 
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching user' });
+  }
+};
+
 // Create a new user
 exports.createUser = async (req, res) => {
     try {
--- Routers/index.js
+++ Routers/index.js
@@ -4,6 +4,7 @@ const authMiddleware = require('../middlwareAuth/Authentication');
 const userController = require('../Controller/UserController');
 
 router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
 router.post('/',  userController.createUser);
 router.put('/:id',  userController.updateUser);
 router.delete('/:id',  userController.deleteUser);
